24172f5f1014ee810aac52696a4a63a709c53360
Narae Kim <knr1210@gmail.com>
2013-06-08 04:49:44 -0500

    trying to grab gps location on map

--- src/com/example/epidemicapp/MainActivity.java
+++ src/com/example/epidemicapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.epidemicapp;
 
+import com.skp.Tmap.TMapGpsManager;
 import com.skp.Tmap.TMapView;
 import com.skp.openplatform.android.sdk.api.APIRequest;
 
@@ -11,17 +12,27 @@ import android.widget.RelativeLayout;
 public class MainActivity extends Activity {
 	private TMapView tmapview = null;
 	private RelativeLayout mapRelativeLayout = null;
+	
+	TMapGpsManager gps = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        APIRequest.setAppKey("68fd1c9f-3a66-30bb-8934-990e8beb01f6");
-
-        tmapview = new TMapView(this);
+        
         RelativeLayout mapRelativeLayout = (RelativeLayout)findViewById(R.id.mapRelativeLayout);
+        
+        tmapview = new TMapView(this);
         mapRelativeLayout.addView(tmapview);
         configureTMapView();
+        
+        gps = new TMapGpsManager(MainActivity.this);
+		gps.setMinTime(1000);
+		gps.setMinDistance(5);
+		
+		gps.setProvider(gps.NETWORK_PROVIDER);
+		gps.OpenGps();
     }
 
     private void configureTMapView() {
@@ -44,4 +55,11 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+	
+	public void onLocationChange(android.location.Location location){
+		double lat = location.getLatitude();
+		double lon = location.getLongitude();
+		
+		
+	}
 }
