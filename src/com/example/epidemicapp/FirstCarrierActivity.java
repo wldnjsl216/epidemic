61c9ef0b86cdf72d3ea4de5d9479a5445b7ef083
Jiwon Choe <brandnew7th@gmail.com>
2013-06-08 14:18:05 -0500

    adding background images

--- src/com/example/epidemicapp/FirstCarrierActivity.java
+++ src/com/example/epidemicapp/FirstCarrierActivity.java
@@ -3,12 +3,14 @@ package com.example.epidemicapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
 
 public class FirstCarrierActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_first_carrier);
 	}
 
